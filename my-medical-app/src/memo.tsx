e8ef64d9aac4c7491fb2021392d6b3bd4756a6f0
takanojun <80045800+takanojun@users.noreply.github.com>
2025-07-10 14:21:15 +0900

    Remove default memos and set page title

--- my-medical-app/src/memo.tsx
+++ my-medical-app/src/memo.tsx
@@ -7,6 +7,11 @@ const params = new URLSearchParams(window.location.search);
 const facilityId = Number(params.get('facilityId')) || 0;
 const facilityName = params.get('facilityName') || '';
 
+// ブラウザタブのタイトルを医療機関名に更新
+if (facilityName) {
+  document.title = facilityName;
+}
+
 createRoot(document.getElementById('memo-root')!).render(
   <React.StrictMode>
     <MemoApp facilityId={facilityId} facilityName={facilityName} />
